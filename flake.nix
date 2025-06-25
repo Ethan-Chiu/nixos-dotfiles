5bbba1373718a6211934a8a6a6f0a6245e17a43a
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-06-25 18:02:00 +0800

    Update nixos version. Add nixos on mac.

--- flake.nix
+++ flake.nix
@@ -1,24 +1,30 @@
 {
-
   description = "Ethan's NixOS setup";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Helix editor
     # helix.url = "github:helix-editor/helix/master";
     # helix.inputs.nixpkgs.follows = "nixpkgs";
   };
-
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
+  outputs = inputs @ { self, nixpkgs, home-manager, nixos-hardware, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
+      nixosmac = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          nixos-hardware.nixosModules.apple-t2
+	];
+      };
       nixos = lib.nixosSystem {
         inherit system;
         modules = [ ./hosts/nixos/configuration.nix ];
@@ -32,6 +38,5 @@
       };
     };
   };
-
 }
 
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,11 +1,11 @@
-{ inputs, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ethan";
   home.homeDirectory = "/home/ethan";
-  
+
   imports = [
     ./helix.nix
   ];
@@ -17,7 +17,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -94,3 +94,4 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
